5b9c685040b75e515c148fff5cc1f73cc026d028
Rittmann <willirittmann@outlook.com>
2023-08-17 13:26:19 -0300

    Creating the Text Components

---
+++ components/src/main/java/com/avantsoft/components/ui/text/Texts.kt
@@ -0,0 +1,57 @@
+package com.avantsoft.components.ui.text
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+
+
+@Composable
+fun TextTitle(
+    text: String,
+    modifier: Modifier = Modifier,
+    maxLines: Int = Int.MAX_VALUE,
+    textAlign: TextAlign? = null,
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        style = MaterialTheme.typography.titleLarge,
+        maxLines = maxLines,
+        textAlign = textAlign,
+    )
+}
+
+@Composable
+fun TextSubtitle(
+    text: String,
+    modifier: Modifier = Modifier,
+    textAlign: TextAlign? = null,
+    maxLines: Int = Int.MAX_VALUE,
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        style = MaterialTheme.typography.titleSmall,
+        textAlign = textAlign,
+        maxLines = maxLines
+    )
+}
+
+
+@Composable
+fun TextBody(
+    text: String,
+    modifier: Modifier = Modifier,
+    maxLines: Int = Int.MAX_VALUE,
+    textAlign: TextAlign? = null,
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        style = MaterialTheme.typography.bodyMedium,
+        maxLines = maxLines,
+        textAlign = textAlign,
+    )
+}
