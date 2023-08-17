c71595dfd865692dddce6b34f6823fce121ad7c4
Rittmann <willirittmann@outlook.com>
2023-08-17 13:28:40 -0300

    Modifying the primary and secondary colors to looks similar to the colors on figma
    - I didn't try to get the exactly color, maybe I'll get it later

--- components/src/main/java/com/avantsoft/components/theme/Color.kt
+++ components/src/main/java/com/avantsoft/components/theme/Color.kt
@@ -6,6 +6,6 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Blue50 = Color(0xFF71ADDD)
+val Green40 = Color(0xFF4CAF50)
 val Pink40 = Color(0xFF7D5260)
--- components/src/main/java/com/avantsoft/components/theme/Theme.kt
+++ components/src/main/java/com/avantsoft/components/theme/Theme.kt
@@ -22,8 +22,8 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
+    primary = Blue50,
+    secondary = Green40,
     tertiary = Pink40
 
     /* Other default colors to override
